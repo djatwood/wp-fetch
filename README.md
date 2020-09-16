31587c292d4a77e31c806b927886c689f288d9a8
Daniel Atwood <daniel@atwood.io>
2020-09-16 13:49:03 -0400

    Complete rewrote everything
    Removed the class format
    
    Removed EventFetch as it was scope creep
    
    Simplified everything. Now I just autofill rewriteable properties to the `init` object. Since data has to be `urlencoded` I provide a `objToURL` function that takes an object and returns an encoded URL string

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WP Fetch
 
-JavaScript fetch wrapper for Wordpress
+A wrapper for [fetch](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) that includes all the nessesary setup for request to [WordPress AJAX](https://codex.wordpress.org/AJAX_in_Plugins).
+
+## Example
+```js
+const init = {
+	body: objToURL({
+		student_id: 1
+	})
+}
+WPFetch(init)
+    .then(r => r.json())
+    .then(console.log)
+    .catch(console.error)
+```
--- wp-fetch.js
+++ wp-fetch.js
@@ -1,39 +1,23 @@
-class WpFetch {
-	constructor(body = {}, data = {}, fetch = true) {
-		this.body = body
-		this.data = data
-		this.url = '/wp-admin/admin-ajax.php?'
-		this.init = {
-			method: 'POST',
-			credentials: 'same-origin',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-			}
-		}
+const defaultInit = {
+	method: 'POST',
+	credentials: 'same-origin',
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+	}
+}
 
-		if (fetch) this.fetch()
+export function WPFetch(init, host = "") {
+	init = Object.assign(defaultInit, init)
+	return fetch(host + '/wp-admin/admin-ajax.php?', init)
+}
+
+export function objToURL(obj) {
+	let list = []
+	for (const k in Object.keys(obj)) {
+		key = encodeURIComponent(k)
+		value = encodeURIComponent(obj[k])
+		list.push(`${key}=${value}`)
 	}
 
-	check(response) {
-		return response.json().then(json => {
-			if (response.ok && json['success']) return json['data']
-
-			const error = json['success'] === false ? JSON.stringify(json['data']) :
-				`${response.status} (${response.statusText})`
-
-			return Promise.reject(new Error(`WpFetch ${error}`))
-		})
-	}
-
-	fetch() {
-		const data = JSON.stringify(this.data)
-		const body = Object.entries(this.body).map((i) => i.map(i => encodeURIComponent(i)).join('=')).join('&')
-
-		this.init.body = `${body}&data=${data}`
-
-		fetch(this.url, this.init)
-			.then(this.check)
-			.then(response => this.success ? this.success(response) : false)
-			.catch(error_message => this.error ? this.error(error_message) : console.error(error_message))
-	}
+	return list.join("&")
 }
